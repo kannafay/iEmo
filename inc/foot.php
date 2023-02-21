7f3e457d5ea7f74750ff3a550233f8665e3b4023
神秘布偶猫 <kannafay@qq.com>
2023-02-21 15:00:59 +0800

    0.1.8

--- inc/foot.php
+++ inc/foot.php
@@ -1,3 +1,11 @@
 </body>
   <script src="<?php echo fileUri(); ?>/assets/js/theme.js"></script>
+  <!-- <script src="<?php //echo fileUri(); ?>/assets/static/nprogress/nprogress.js"></script>
+  <script>
+    NProgress.start();
+    $(document).ready(()=>{
+      NProgress.done();
+    })
+    NProgress.remove();
+  </script> -->
 </html>
