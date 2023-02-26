77824b2d002173421f67b67887e6c43f7589c7b8
神秘布偶猫 <kannafay@qq.com>
2023-02-26 21:02:26 +0800

    0.3.3

--- inc/head.php
+++ inc/head.php
@@ -8,13 +8,13 @@
   <meta name="renderer" content="webkit" />
   <meta name="apple-mobile-web-app-capable" content="yes" />
   <meta name="viewport" content="width=device-width,height=device-height,initial-scale=1.0,maximum-scale=1.0,user-scalable=no" />
-  <meta name="keywords" content="<?php bloginfo('name'); ?>" />
-  <meta name="description" content="<?php bloginfo('description'); ?>" />
-  <title><?php if(function_exists('show_wp_title')){show_wp_title();} ?></title>
-  <link rel="shortcut icon" href="<?php if(has_site_icon()) echo site_icon_url(); else echo fileUri().'/assets/images/wp-logo-blue.png'; ?>" type="image/x-icon">
-  <link rel="stylesheet" href="<?php echo fileUri(); ?>/assets/css/style.css">
-  <link rel="stylesheet" href="<?php echo fileUri(); ?>/assets/static/iconfont/iconfont.css">
-  <script src="<?php echo fileUri(); ?>/assets/js/jquery.min.js"></script>
+  <meta name="keywords" content="<?=is_home() ? bloginfo('name') : the_title(); ?>" />
+  <meta name="description" content="<?=bloginfo('description'); ?>" />
+  <title><?=function_exists('show_wp_title') ? show_wp_title() : bloginfo('name'); ?></title>
+  <link rel="shortcut icon" href="<?=has_site_icon() ? site_icon_url() : fileUri().'/assets/images/wp-logo-blue.png'; ?>" type="image/x-icon">
+  <link rel="stylesheet" href="<?=fileUri(); ?>/assets/css/style.css">
+  <link rel="stylesheet" href="<?=fileUri(); ?>/assets/static/iconfont/iconfont.css">
+  <script src="<?=fileUri(); ?>/assets/js/jquery.min.js"></script>
   <!-- <link rel="stylesheet" href="<?php //echo fileUri(); ?>/assets/static/nprogress/nprogress.css"> -->
   <?php
     if(get_option("iemo_page_animation")) { ?>
@@ -25,7 +25,7 @@
         .category main .content article .cate-box ul li,
         .tag main .content article .tag-box ul li,
         .search main .content article .search-box ul li {
-          animation: FadeIn-<?php echo get_option("iemo_page_animation"); ?> .5s forwards !important;
+          animation: FadeIn-<?=get_option("iemo_page_animation"); ?> .5s forwards !important;
         }
       </style>
     <?php }
